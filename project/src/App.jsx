3a4a652f095d0aceb6a18c5c056dec4c5a7af48d
Udit Mehta <uditmehta100@gmail.com>
2024-03-12 08:04:16 +0530

    checking the commit

--- project/src/App.jsx
+++ project/src/App.jsx
@@ -3,14 +3,12 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import Navbar from './components/navbar'
 
-
 function App() {
   return (
     <div>
       <Navbar />
       <h1>hiii</h1>
     </div>
-    
   )
 }
 
