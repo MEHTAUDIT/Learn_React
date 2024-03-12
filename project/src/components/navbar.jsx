11c7c450b020dfd346c8e511b3689753bb781cc1
Udit Mehta <uditmehta100@gmail.com>
2024-03-12 20:39:04 +0530

    navbar

--- project/src/components/navbar.jsx
+++ project/src/components/navbar.jsx
@@ -1,10 +1,18 @@
 function Navbar(props)
 {
   return (
-    <div>
-      <p>{props.isloggedin ? "welcome":"Please login"}</p>
-    </div>
-
+    
+    <ul className="navbar">
+      <li className="nav-item">
+        <div className="nav-link">About</div>
+      </li>
+      <li className="nav-item">
+        <div className="nav-link">Product</div>
+      </li>
+      <li className="nav-item">
+        <div className="nav-link">Contact</div>
+      </li>
+    </ul>
   );
 
 }
