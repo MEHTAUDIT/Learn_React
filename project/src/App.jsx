20d450e16df10da3fdef178b2fb9c788da8dd8a6
Udit Mehta <uditmehta@Udits-MacBook-Air.local>
2024-03-12 07:48:34 +0530

    simple react

---
+++ project/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import Navbar from './components/navbar'
+
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>hiii</h1>
+    </div>
+    
+  )
+}
+
+export default App
