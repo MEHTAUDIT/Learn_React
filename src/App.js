48bab0ecc39bd93bb0e76243cb9efa17eded8e9c
Udit Mehta <uditmehta100@gmail.com>
2024-04-12 10:11:11 +0530

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,12 @@
-import { useState } from 'react';
-import AppContext from './store/app-context';
 import Login from './components/login';
+import AppProvider from './store/AppProvider';
 
 function App() {
-  // const ctx = useContext(AppContext);
-  const [isLoggedin, setIsLoggedin] = useState(false);
 
-  function handlelogin() {
-    setIsLoggedin(true);
-  }
-
-  return <AppContext.Provider value={{isLoggedin:isLoggedin,handlelogin:handlelogin}}>
+  return <AppProvider>
     <h1>Hello Context</h1>
     <Login />
-  </AppContext.Provider>;
+  </AppProvider>;
 }
 
 export default App;
--- src/store/AppProvider.js
+++ src/store/AppProvider.js
@@ -1,16 +1,48 @@
-import React,{useState} from "react";
+import React,{useReducer} from "react";
+import AppContext from "./app-context";
+
+function authReducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return {isLoggedin: true};
+    case "LOGOUT":
+      return {isLoggedin: false};
+    default:
+      return state;
+  }
+}
+
+const initialState = {
+  isLoggedin: false
+};
 
 function AppProvider({children}){
-  const [isLoggedin, setIsLoggedin] = useState(false);
+
+  const [authState, dispathchAuth] = useReducer(authReducer, initialState);
 
   function handlelogin() {
-    setIsLoggedin(true);
+    dispathchAuth({
+        type: "LOGIN"
+        
+    })
+    // setIsLoggedin(true);
+  }
+
+  function handlelogout() {
+    dispathchAuth({
+        type: "LOGOUT"
+        
+    })
+    // setIsLoggedin(false);
   }
 
   const appcontextvalue = {
-    isLoggedin:isLoggedin,
-    handlelogin:handlelogin
+    isLoggedin:authState.isLoggedin,
+    handlelogin:handlelogin,
+    handlelogout:handlelogout
   }
 
-  return <AppProvider.Provider value={appcontextvalue}>{children}</AppProvider.Provider>;
+  return <AppContext.Provider value={appcontextvalue}>{children}</AppContext.Provider>;
 }
+
+export default AppProvider;
